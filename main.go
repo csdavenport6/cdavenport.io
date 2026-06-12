7eb9cf9e269fe4f6d0fba3da43e2101f320de71c
Connor Davenport <cdavenport626@gmail.com>
2026-06-11 23:25:04 -0700

    remove endpoints

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"embed"
-	"io/fs"
 	"log"
 	"net/http"
 )
-
-//go:embed static
-var staticFiles embed.FS
-
-//go:embed Connor-Davenport-Engineering.pdf
-var resumePDF []byte
 
 func main() {
 	srv, err := NewServer("posts", "templates")
@@ -19,21 +11,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	staticFS, err := fs.Sub(staticFiles, "static")
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	mux := http.NewServeMux()
 	mux.HandleFunc("/healthz", srv.HandleHealth)
-	mux.HandleFunc("/resume", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/pdf")
-		w.Header().Set("Content-Disposition", `inline; filename="Connor-Davenport-Engineering.pdf"`)
-		_, _ = w.Write(resumePDF)
-	})
 	mux.HandleFunc("/", srv.HandleIndex)
 	mux.HandleFunc("/posts/", srv.HandlePost)
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(staticFS))))
 
 	log.Println("listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
